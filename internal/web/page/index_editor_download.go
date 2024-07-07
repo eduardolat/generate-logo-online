d6fa7f841b34c037994debc74a83f00261ab01e8
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-07-07 13:49:31 -0600

    chore: Update index_editor_download.go to use Button component for download button

--- internal/web/page/index_editor_download.go
+++ internal/web/page/index_editor_download.go
@@ -4,20 +4,21 @@ import (
 	"github.com/eduardolat/generate-logo/internal/web/component"
 	lucide "github.com/eduardolat/gomponents-lucide"
 	"github.com/maragudk/gomponents"
-	"github.com/maragudk/gomponents/components"
 	"github.com/maragudk/gomponents/html"
 )
 
 func indexEditorDownload() gomponents.Node {
 	return html.Div(
-		html.Button(
-			components.Classes{
-				"bg-black rounded-b w-full py-2":      true,
-				"text-white font-bold cursor-pointer": true,
-				"flex justify-center items-center":    true,
+		component.Button(component.ButtonParams{
+			Type:  component.ButtonTypeButton,
+			Color: component.ColorBlack,
+			Size:  component.SizeMd,
+			Block: true,
+			Class: "rounded-t-none",
+			Children: []gomponents.Node{
+				component.SpanText("Download"),
+				lucide.CloudDownload(html.Class("ml-1")),
 			},
-			component.SpanText("Download"),
-			lucide.CloudDownload(html.Class("ml-1")),
-		),
+		}),
 	)
 }
