13a9bf81cdf3b5020efe735ff50e33b67a4ceecd
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-08-10 21:40:30 -0600

    Update search icon layout to use grid instead of flex

--- internal/web/page/search_icon.go
+++ internal/web/page/search_icon.go
@@ -30,15 +30,16 @@ func (h *handlers) searchIconHandler(c echo.Context) error {
 
 func searchIconResults(foundIcons []icons.Icon) gomponents.Node {
 	return html.Div(
-		html.Class("flex flex-wrap gap-2"),
+		html.Class("grid grid-cols-7 gap-2"),
 		gomponents.Group(
 			gomponents.Map(
 				foundIcons,
 				func(icon icons.Icon) gomponents.Node {
 					return html.Div(
+						html.Class("tooltip"),
 						html.Button(
 							html.Type("button"),
-							html.Class("btn btn-outline btn-square btn-lg"),
+							html.Class("btn btn-outline btn-square btn-lg w-full"),
 							alpine.XOn("click", `setOriginalSVG('`+icon.SVG+`')`),
 							html.Title(icon.Name),
 							icon.Icon(html.Class("size-8")),
